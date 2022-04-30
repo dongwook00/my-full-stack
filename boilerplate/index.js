3ebf38b691308faeb21ec497fd8b6df956936ddc
Dongwook Kim <dongwook227@gmail.com>
2022-04-30 11:33:21 -0400

    add db connection

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const port = 3000;
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.padg1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("MongoDB connected..."))
+  .catch((error) => console.error(error));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
