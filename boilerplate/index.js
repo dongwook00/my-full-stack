7cbfa0c53b486f06fd460728ae5ea60f7811bff7
Dongwook Kim <dongwook227@gmail.com>
2022-05-01 17:19:20 -0400

    add register

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 require("dotenv").config();
-
 const port = 3000;
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.padg1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
@@ -12,5 +17,17 @@ mongoose
   .catch((error) => console.error(error));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((error, userInfo) => {
+    if (error) {
+      return res.json({ success: false, error });
+    }
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
