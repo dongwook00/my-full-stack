4c08aeea1941a8e1227ed96b1fc4265e9f5ec3fd
Dongwook Kim <dongwook227@gmail.com>
2022-05-11 09:45:50 -0400

    add router for users

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -5,8 +5,6 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const port = 3000;
-const { auth } = require("./middleware/auth");
-const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -19,69 +17,6 @@ mongoose
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.post("/apu/users/register", (req, res) => {
-  const user = new User(req.body);
-  user.save((error, userInfo) => {
-    if (error) {
-      return res.json({ success: false, error });
-    }
-    return res.status(200).json({
-      success: true,
-    });
-  });
-});
-
-app.post("/api/users/login", (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user) {
-      return res.json({
-        loginSuccess: false,
-        message: "no user",
-      });
-    }
-
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch) {
-        return res.json({ loginSuccess: false, message: "wrong password" });
-      }
-
-      //token
-      user.generateToken((err, user) => {
-        if (err) {
-          return res.status(400).send(err);
-        }
-
-        res
-          .cookie("x_auth", user.token)
-          .status(200)
-          .json({ loginSuccess: true, userId: user._id });
-      });
-    });
-  });
-});
-
-app.get("/api/users/auth", auth, (req, res) => {
-  console.log("auth");
-  res.status(200).json({
-    _id: req.user._id,
-    isAdmin: req.user.role === 0 ? false : true,
-    isAuth: true,
-    email: req.user.email,
-    name: req.user.name,
-    lastname: req.user.lastname,
-    role: req.user.role,
-    image: req.user.image,
-  });
-});
-
-app.get("/api/users/logout", auth, (req, res) => {
-  console.log("logout");
-  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).send({
-      success: true,
-    });
-  });
-});
+app.use("/api/users", require("./routes/users"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
