fe7a210cce3e40f3e8917d020ccfa085d97ef626
Dongwook Kim <dongwook227@gmail.com>
2022-05-14 06:23:28 -0400

    add db connection

--- app/server/index.js
+++ app/server/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./config/key");
 
 const port = 3000;
 const app = express();
+
+mongoose
+  .connect(config.DB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => res.send("Hello world"));
 
