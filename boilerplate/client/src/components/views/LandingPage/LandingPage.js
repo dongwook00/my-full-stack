b720da6b3a47c7f602d329b985b633b1d6793151
Dongwook Kim <dongwook227@gmail.com>
2022-05-15 06:57:04 -0400

    setup proxy middleware for cors

--- boilerplate/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 export default function LandingPage() {
+  useEffect(() => {
+    axios
+      .get("/api/hello")
+      .then((response) => console.log(response.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return <div>LandingPage</div>;
 }
