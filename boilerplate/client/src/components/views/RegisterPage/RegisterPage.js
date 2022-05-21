c3b18a7ab6a3018002ce92eaff118a58d58b3612
Dongwook Kim <dongwook227@gmail.com>
2022-05-21 06:53:14 -0400

    add `RegisterPage` view

--- boilerplate/client/src/components/views/RegisterPage/RegisterPage.js
+++ boilerplate/client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,5 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, name, password);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit}
+      >
+        <label>Email</label>
+        <input type="email" value={email} onChange={handleEmailChange} />
+
+        <label>Name</label>
+        <input type="text" value={name} onChange={handleNameChange} />
+
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={handleConfirmPasswordChange}
+        />
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
