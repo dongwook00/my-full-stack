3976f81d698426646d28bc3bac17d8a7b4a4fcba
Dongwook Kim <dongwook227@gmail.com>
2022-05-21 16:11:01 -0400

    add logout feature

--- boilerplate/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate/client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-export default function LandingPage() {
+export default function LandingPage(props) {
   useEffect(() => {
     axios
       .get("/api/hello")
@@ -9,5 +9,29 @@ export default function LandingPage() {
       .catch((err) => console.error(err));
   }, []);
 
-  return <div>LandingPage</div>;
+  const handleLogout = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to logout");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h2>Landing Page</h2>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
 }
